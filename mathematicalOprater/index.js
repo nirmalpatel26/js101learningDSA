755f2d4d79598d9b6eb53af8ac2ab7d66f25ad5c
nirmal9977patle <nirmalpatel@gmail.com>
2023-01-03 13:47:47 +0530

    first commit

---
+++ mathematicalOprater/index.js
@@ -0,0 +1,9 @@
+let name;
+name="Nirmal";
+let father_name;
+father_name="Rajendra";
+console.log("Name",name);
+console.log("Father_name",father_name);
+let x=3;
+let y=1;
+console.log(((x+y)**(x-y))*((y**2)+(x**3)));
